357d29ca34c1a8394911b034f1b5fe626fcfc7ca
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ JS/views/login.js
@@ -0,0 +1,24 @@
+﻿TestApp.Login = function (params) {
+    function btnLogin_OnClick(e) {
+        TestApp.app.UserName = loginModel.txtUserNameSettings.value();
+        TestApp.app.Password = loginModel.txtPasswordSettings.value();
+        var uri = TestApp.app.router.format({
+            view: 'Index'
+        });
+        TestApp.app.navigate(uri);
+    }
+    var loginModel = {
+        txtUserNameSettings: {
+            value: ko.observable(TestApp.app.UserName)
+        },
+        txtPasswordSettings: {
+            value: ko.observable(TestApp.app.Password),
+            mode: 'password'
+        },
+        btnLoginSettings: {
+            text: 'Login',
+            clickAction: btnLogin_OnClick
+        }
+    };
+    return loginModel;
+};
